a488211906218e69d0eca99ba4b80a833170d4cc
frontends2 <jaggapong2548@gmail.com>
2025-11-25 15:47:00 +0700

    feat: add version control and docker demo page

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -11,3 +11,20 @@
 <script setup>
 //
 </script>
+<template>
+  <div class="q-pa-md">
+    <q-checkbox v-model="val" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      val: ref(true)
+    }
+  }
+}
+</script>
